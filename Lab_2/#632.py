ecd0795b0a10893d3e0683a9f9949f671ce1de0d
Avipshy <denibuuu@gmail.com>
2026-04-04 11:11:09 +1000

    Fix 632

--- Lab_2/#632.py
+++ Lab_2/#632.py
@@ -5,25 +5,25 @@ class MyCircularQueue:
         self.size = k
         
     def enQueue(self, value: int) -> bool:
-        if len(self.queue) < self.size:
+        if not self.isFull():
             self.queue.append(value)
             return True
         return False
 
     def deQueue(self) -> bool:
-        if len(self.queue) != 0:
+        if not self.isEmpty():
             self.queue.pop(0)
             return True
         return False
 
     def Front(self) -> int:
-        if len(self.queue) != 0:
+        if not self.isEmpty():
             return self.queue[0]
         else:
             return -1
 
     def Rear(self) -> int:
-        if len(self.queue) != 0:
+        if not self.isEmpty():
             return self.queue[-1]
         else:
             return -1
